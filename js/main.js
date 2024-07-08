d022cbb0d8d808d5bcd11c8139b1e3b01afd8663
mberns <martiberns@gmail.com>
2024-07-08 20:07:29 -0300

    agrego login registro y usuario - falta terminar

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 document.getElementById("header").innerHTML=` <nav id="nav-header" class="fondo navbar navbar-expand-sm navbar-expand-lg navbar-light ">
 <div class="container fondo">
-  <a class="navbar-brand fondo" href="index.html">Inicio</a>
-  <button class="navbar-toggler d-lg-none fondo" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-  </button>
+  
+
   <div class="collapse navbar-collapse fondo" id="collapsibleNavId">
       <ul class="navbar-nav me-auto mt-2 mt-lg-0 fondo">
+          <li class="nav-item fondo">
+                <p id="codo-a-codo" class="nav-link fondo">Codo a Codo</p>
+          </li>  
           <li class="nav-item fondo">
               <a class="nav-link fondo" href="index.html" aria-current="page">Home <span class="visually-hidden">(current)</span></a>
           </li>
           <li class="nav-item fondo">
-              <a class="nav-link fondo" href="#">Link</a>
+              <a class="nav-link fondo" href="registro.html">Registro</a>
+          </li>
+          <li class="nav-item fondo">
+              <a class="nav-link fondo" href="login.html">Login</a>
           </li>
           <li class="nav-item dropdown fondo">
-              <a class="nav-link dropdown-toggle fondo" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">CRUD</a>
+              <a class="nav-link dropdown-toggle fondo" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Adminitración</a>
               <div class="dropdown-menu fondo" aria-labelledby="dropdownId">
-                  <a class="dropdown-item fondo" href="libros.html">Libros</a>
-                  <a class="dropdown-item fondo" href="#">Action 2</a>
+                  <a class="dropdown-item fondo" href="libros.html">CRUD Libros</a>
               </div>
           </li>
       </ul>
@@ -30,3 +32,22 @@ document.getElementById("header").innerHTML=` <nav id="nav-header" class="fondo 
 </nav>
 `
 //bg-light
+/*
+<a class="navbar-brand fondo" href="index.html">Biblioteca</a>
+  <button class="navbar-toggler d-lg-none fondo" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+      aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+
+
+ <li class="nav-item dropdown fondo">
+              <a class="nav-link dropdown-toggle fondo" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Adminitración</a>
+              <div class="dropdown-menu fondo" aria-labelledby="dropdownId">
+                  <a class="dropdown-item fondo" href="libros.html">Libros</a>
+                  <a class="dropdown-item fondo" href="#">Action 2</a>
+              </div>
+          </li>
+
+
+
+*/
